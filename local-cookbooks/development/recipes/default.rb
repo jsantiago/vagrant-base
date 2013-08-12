7f666df7949788a882683e9bdf621f49658ace1f
jsantiago <joshs@slalom.com>
2013-08-12 18:48:39 -0500

    include ssh-key + dotfiles recipe

--- local-cookbooks/development/recipes/default.rb
+++ local-cookbooks/development/recipes/default.rb
@@ -1,2 +1,8 @@
 # provisions global node packages
 include_recipe 'development::npm'
+
+# generate ssh-key
+include_recipe 'development::ssh-key'
+
+# install dotfiles
+include_recipe 'development::dotfiles'
