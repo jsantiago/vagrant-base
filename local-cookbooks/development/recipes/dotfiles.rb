c0da068e5e9f17ec7262f72e1dfa9d83fb985654
jsantiago <joshs@slalom.com>
2013-08-13 14:22:50 -0500

    check if dotfiles already installed

--- local-cookbooks/development/recipes/dotfiles.rb
+++ local-cookbooks/development/recipes/dotfiles.rb
@@ -1,4 +1,5 @@
 execute "install dotfiles" do
     command "su -l vagrant -c 'cd /home/vagrant/.dotfiles && bash -i ./dotfiles'"
     action :run
+    not_if { ::File.exists?("/home/vagrant/.vimrc") }
 end
